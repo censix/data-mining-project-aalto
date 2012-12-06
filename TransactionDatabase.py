6e72ab7aba505718124e56c19102b7040b3b5429
antti.hirvonen@gmail.com <antti.hirvonen@gmail.com@bf1b6a61-639f-1aa9-00f3-7b989d8e7bec>
2012-12-06 14:00:55 +0000

    Implemented transaction database loading and iterator functionality

--- TransactionDatabase.py
+++ TransactionDatabase.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+import csv
 
 # This behaves like a class:
 # my_transaction = Transaction(id=2, itemset=['a','b','c'], label=1)
@@ -10,9 +11,10 @@ class TransactionDatabase(object):
     """
     Class for storing transactions.
     """
-    def __init__(self):
+    def __init__(self, minsup):
         # List of Transactions
         self.transactions = []
+        self.minimumSupport = minsup
 
     def buildConditionalDatabase(self, pattern):
         """
@@ -35,3 +37,26 @@ class TransactionDatabase(object):
         Removes all transactions from the database that match the given pattern.
         """
         pass
+
+    @staticmethod
+    def loadFromFile(filename, minsup):
+        """
+        Loads transactions from CSV file of form
+        id,itemset,label
+
+        id should be transaction number, itemset contain items separated by a space (like "a b c")
+        """
+        database = TransactionDatabase(minsup)
+
+        for line in csv.reader(open(filename)):
+            t = Transaction(line[0], line[1].split(" "), line[2])
+            database.transactions.append(t)
+
+        return database
+
+    def __repr__(self):
+        return "\n".join([repr(transaction) for transaction in self.transactions])
+
+    def __iter__(self):
+        for transaction in self.transactions:
+            yield transaction
