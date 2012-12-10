e2badddb356b917f9d159da01386f58db507f902
antti.hirvonen@gmail.com <antti.hirvonen@gmail.com@bf1b6a61-639f-1aa9-00f3-7b989d8e7bec>
2012-12-10 20:38:17 +0000

    Added some basic tests for TransactionDatabase

--- tests.py
+++ tests.py
@@ -14,5 +14,46 @@ class TransactionTestCase(unittest.TestCase):
         # Contains one
         self.assertFalse(t.contains([3, 4]))
 
+class TransactionDatabaseTestCase(unittest.TestCase):
+    def setUp(self):
+        self.database = TransactionDatabase(1)
+        self.database.add(Transaction(1, [1,2,3], 0))
+        self.database.add(Transaction(2, [2,3,4], 1))
+        self.database.add(Transaction(3, [1], 0))
+        self.database.add(Transaction(4, [2], 1))
+        self.database.add(Transaction(5, [2,3, 5], 1))
+
+    def test_len(self):
+        self.assertEquals(len(self.database), 5)
+
+    def test_buildConditionalDatabase(self):
+        cond_db = self.database.buildConditionalDatabase([1])
+        self.assertEquals(len(cond_db), 2)
+
+    def test_transactionListFromPattern(self):
+        tlist = self.database.transactionListFromPattern([1])
+        self.assertEquals(tlist, [1, 3])
+
+    def test_labelSupport(self):
+        self.assertEquals(self.database.labelSupport(), 0.6)
+
+    def test_labelAndPatternSupport(self):
+        # All matched
+        self.assertEquals(self.database.labelAndPatternSupport([2]), 0.6)
+        # One matched
+        self.assertEquals(self.database.labelAndPatternSupport([5]), 0.2)
+
+    def test_patternSupport(self):
+        self.assertEquals(self.database.patternSupport([2,3,5]), 0.2)
+
+    def test_removeTransactions(self):
+        self.database.removeTransactions([3, 5])
+        self.assertEquals(len(self.database), 3)
+
+    def test_cleanAndPrune(self):
+        # Not needed anymore? Add tests later
+        pass
+
+
 if __name__ == '__main__':
     unittest.main()
