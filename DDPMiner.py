c1a1c2c2695db08190a66963bce31787318210dc
antti.hirvonen@gmail.com <antti.hirvonen@gmail.com@bf1b6a61-639f-1aa9-00f3-7b989d8e7bec>
2012-12-10 20:51:26 +0000

    Flag for turning debug prints on / off

--- DDPMiner.py
+++ DDPMiner.py
@@ -7,17 +7,18 @@ class DDPMine:
     """
     Main class implementing the DDPMine algorithm.
     """
-    
-    def __init__(self):
+
+    def __init__(self, debug=False):
         self.fp_tree = FPTree()
         self._maxGain_ = 0.0
         self._bestPattern = None
         self._bestPatterns = []
+        self.debug = debug
 
     def mine(self,transactionDatabase,support):
         self._globalTransactionDatabase = transactionDatabase
         self.fp_tree = self.buildTree(transactionDatabase)
-        
+
         return self._mine(self.fp_tree,support)
 
     def _mine(self,P,s):
@@ -25,76 +26,79 @@ class DDPMine:
         Does the heavy lifting of mining for the nodes – returns the list of most
         discriminative patterns.
         """
-        #while the tree is not empty 
+        #while the tree is not empty
         while not P.empty :
-            
+
             #branch and bound to find best pattern
             self.branchAndBound(P,s,[])
-            
-            print "best pattern:"
-            print self._bestPattern
-            print "Max Gain:"
-            print self._maxGain_
-            
+
+            if self.debug:
+                print "best pattern:"
+                print self._bestPattern
+                print "Max Gain:"
+                print self._maxGain_
+
             #if no best pattern then break
             if self._bestPattern == None:
                 break
-            
+
             #get transaction list and update tree
             transactionList = self._globalTransactionDatabase.transactionListFromPattern(self._bestPattern)
-            
+
             P.UpdateTree(transactionList)
-            
+
             #append the new best found pattern
             self._bestPatterns.append(self._bestPattern)
-            
+
             #reset the best pattern
             self._bestPattern = None
             self._maxGain_ = 0
-            
+
         #return the list of best patterns
         return self._bestPatterns
-    
+
     def buildTree(self,transactionDatabase):
-        
+
         master = FPTree()
         for transaction in transactionDatabase:
             master.add(transaction)
-            
+
         return master
-    
+
     def branchAndBound(self,tree,minimum_support,suffix):
-        
+
         for item, nodes in tree.items():
-            
+
             #make sure the support is sufficient
             support = sum(n.count for n in nodes)
             if support >= minimum_support and item not in suffix:
                 #we found a new possible canidate
                 found_set = [item] + suffix
-                print "found set:"
-                print found_set
-                
+                if self.debug:
+                    print "found set:"
+                    print found_set
+
                 #since we are looking for the best pattern globally we need to compute the pattern's global information gain thus
                 #we use the global transaction database (this is how we get around creating the conditional database right away)
                 #complexity of this step is 2N where N is size of global database
                 infoGain = UtilityMethods.InformationGain(self._globalTransactionDatabase.patternSupport(found_set),self._globalTransactionDatabase.labelSupport(),self._globalTransactionDatabase.labelAndPatternSupport(found_set))
-                print "info gain:"
-                print infoGain
-                
+                if self.debug:
+                    print "info gain:"
+                    print infoGain
+
                 #if it is the best pattern then save it
                 if infoGain > self._maxGain_ :
                     self._maxGain_ = infoGain
                     self._bestPattern = found_set
-                
+
                 #construct the conditional database of new canidate from the global database
                 #complexity of this step is probably N
                 conditionalDatabase = self._globalTransactionDatabase.buildConditionalDatabase(found_set)
-                
+
                 #compute the information gain upperbound of this new conditional database based on the size of the conditional database and the global label support
                 #complexity of this step should be 1
-                infoGainBound = UtilityMethods.InformationGainUpperBound(conditionalDatabase.size()/self._globalTransactionDatabase.size(),self._globalTransactionDatabase.labelSupport())    
-                
+                infoGainBound = UtilityMethods.InformationGainUpperBound(conditionalDatabase.size()/self._globalTransactionDatabase.size(),self._globalTransactionDatabase.labelSupport())
+
                 #if potential for high information gain patterns then recursivly mine them
                 if self._maxGain_ >= infoGainBound :
                     pass
