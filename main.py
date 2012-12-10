c1c1fb24577a1db478e634d7c9c56dafec5d836a
antti.hirvonen@gmail.com <antti.hirvonen@gmail.com@bf1b6a61-639f-1aa9-00f3-7b989d8e7bec>
2012-12-10 20:51:05 +0000

    Better main program (custom options, change input filename)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from DDPMiner import DDPMine
 from fptree import FPTree
 from TransactionDatabase import TransactionDatabase
+from optparse import OptionParser
 
 def run_ddpmine():
     # Just some placeholder data
@@ -8,13 +9,23 @@ def run_ddpmine():
     miner.mine()
 
 if __name__ == "__main__":
-    
-    database = TransactionDatabase.loadFromFile("test.csv")
-    database.cleanAndPrune(2)
-    
-    miner = DDPMine()
+    usage = "usage: %prog [options] filename"
+    parser = OptionParser(usage)
+    parser.add_option("-d", "--debug",
+                      action="store_true", dest="debug")
+
+    (options, args) = parser.parse_args()
+
+    if len(args) != 1:
+        parser.error("You must give the filename.")
+
+    print "Mining from file %s..." % args[0]
+    database = TransactionDatabase.loadFromFile(args[0])
+    #database.cleanAndPrune(2)
+
+    miner = DDPMine(debug=options.debug)
     print miner.mine(database,0)
-    
+
     """
     database = TransactionDatabase.loadFromFile("test.csv")
     database.cleanAndPrune(2)
